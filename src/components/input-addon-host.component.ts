1f9fa785c89bf7e043004ad1b6a71705c2a53081
Sherif Elmetainy <sherif.elmetainy@code-art.net>
2018-02-16 18:03:15 +0200

    Add rtl support for input group addon.

--- src/components/input-addon-host.component.ts
+++ src/components/input-addon-host.component.ts
@@ -1,19 +1,46 @@
-﻿import { Component, Inject, Renderer2, ElementRef, AfterViewInit, ViewChild } from "@angular/core";
+﻿import { Component, Inject, Renderer2, ElementRef, AfterViewInit, ViewChild, OnDestroy } from "@angular/core";
+import { CANG_CULTURE_SERVICE, ICultureService } from "@code-art/angular-globalize";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
-    templateUrl: './templates/input-addon-host.component.html',
-    styleUrls: ['./styles/input-addon-host.component.less']
+    templateUrl: './templates/input-addon-host.component.html'
 })
-export class InputAddonHostComponent implements AfterViewInit {
+export class InputAddonHostComponent implements AfterViewInit, OnDestroy {
 
     @ViewChild("inputhost") inputHost: ElementRef;
     hostedElement: ElementRef;
+    private readonly sub: Subscription;
 
-    constructor(@Inject(Renderer2) private readonly renderer: Renderer2) {
+    constructor(@Inject(CANG_CULTURE_SERVICE) private readonly cultureService: ICultureService,
+        @Inject(Renderer2) private readonly renderer: Renderer2) {
         this.type = 'calendar';
+
+        this.sub = this.cultureService.cultureObservable.subscribe(o => {
+            this.applyStyles();
+        });
+    }
+
+    private applyStyles(): void {
+        if (this.hostedElement) {
+            let s = this.getRtlControlStyle();
+            let rtl = this.cultureService.isRightToLeft();
+            for (let key in s) {
+                if (s.hasOwnProperty(key)) {
+                    if (rtl) {
+                        this.renderer.setStyle(this.hostedElement.nativeElement, key, s[key]);
+                    } else {
+                        this.renderer.removeStyle(this.hostedElement.nativeElement, key);
+                    }
+                }
+            }
+        }
     }
 
     type: string;
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
 
     ngAfterViewInit(): void {
 
@@ -36,5 +63,29 @@ export class InputAddonHostComponent implements AfterViewInit {
                 this.renderer.appendChild(oldParent, parent);
             }
         }
+        this.applyStyles();
+    }
+
+    getRtlControlStyle(): { [key: string]: string } {
+        return {
+            'border-top-right-radius': '4px',
+            'border-bottom-right-radius': '4px',
+            'border-top-left-radius': '0px',
+            'border-bottom-left-radius': '0px',
+        };
+    }
+
+    getAddonStyle(): { [key: string]: string } {
+        if (this.cultureService.isRightToLeft()) {
+            return {
+                'border-top-right-radius': '0',
+                'border-bottom-right-radius': '0',
+                'border-top-left-radius': '4px',
+                'border-bottom-left-radius': '4px',
+                'border-right-width': '0',
+                'border-left': '1px solid rgb(204, 204, 204)'
+            };
+        }
+        return {};
     }
 }
