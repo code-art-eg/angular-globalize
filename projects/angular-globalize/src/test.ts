1254a9d4ea60df1ea3f76e23f0a8f36774cf104e
sherif-elmetainy <sherif.elmetainy@gmail.com>
2023-08-31 02:02:36 +0300

    Update to angular 15

--- projects/angular-globalize/src/test.ts
+++ projects/angular-globalize/src/test.ts
@@ -12,13 +12,6 @@ import 'globalize/currency';
 import 'globalize/date';
 import 'globalize/number';
 
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -26,8 +19,4 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
 
